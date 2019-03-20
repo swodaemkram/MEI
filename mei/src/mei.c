5cade6d909647269653d7362450cc9b167e8db07
mark <mark@mark@kyin.org>
2019-03-20 13:43:41 -0400

    test

---
+++ mei/src/mei.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : mei.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : Fireking Security Group
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("mei"); /* prints mei */
+	return EXIT_SUCCESS;
+}
